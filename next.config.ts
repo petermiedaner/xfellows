8e3177589a39ce89a4f975e5ee5691142a908b13
Peter Miedaner <miedaner@mit.edu>
2026-05-10 11:30:48 -0400

    images

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
   basePath: isProd ? '/xfellows' : '',
-  assetPrefix: isProd ? '/xfellows/' : '',
   images: {
     unoptimized: true,
   },
