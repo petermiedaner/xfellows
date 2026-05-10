91035b1d223bb322d1fede274dbe90020b5a78b9
Peter Miedaner <miedaner@mit.edu>
2026-05-10 11:22:17 -0400

    try again

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/xfellows',
-}
+  images: {
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
