2b3b889beffeef4e2c3b52303f4ff12566eeff90
Peter Miedaner <miedaner@mit.edu>
2026-05-11 15:56:54 -0400

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,6 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         flexDirection: "column",
         minHeight: "100vh",
       }}>
+        <GoogleAnalytics gaId="G-XDJJJ0MBH7" />
         <Navbar />
 
         {/* Main content grows to push footer down */}
